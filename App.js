828edbe5bb29239e080e8aaa1375839335aadde5
Satyam-Muluk <73930297+Satyam-Muluk@users.noreply.github.com>
2024-11-06 19:09:05 +0530

    basics

---
+++ App.js
@@ -0,0 +1,33 @@
+ 
+ 
+//  creating like this structure
+//  <div id="parent">
+//     <div id="child">
+//         <h1>heading</h1>
+        //   <h2> heading 2 </h2>
+//     </div>
+{/* <div id="child2">
+//         <h1>heading</h1>
+        //   <h2> heading 2 </h2>
+//     </div> */}
+//  </div>
+ 
+const parent = React.createElement("div",{id:"parent"},
+    [
+    React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"Nested heading"),React.createElement("h2",{},"Sibling heading")]
+    ),
+    React.createElement("div",{id:"child2"},
+        [React.createElement("h1",{},"Nested heading 2"),React.createElement("h2",{},"Sibling heading 2")]
+    ),
+]
+);
+
+ 
+ 
+ const heading = React.createElement("h1",{id:"heading"},"Hello World in React");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent)
+
+// root.render(heading)
