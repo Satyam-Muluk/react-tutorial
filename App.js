4772f65e346f226fb7af8d4f028fa1d8e8812bca
Satyam-Muluk <muluksatyam7@gmail.com>
2024-11-18 18:24:38 +0530

    components learned

--- App.js
+++ App.js
@@ -1,33 +1,40 @@
- import React from "react";
- import ReactDOM from "react-dom/client";
-//  creating like this structure
-//  <div id="parent">
-//     <div id="child">
-//         <h1>heading</h1>
-        //   <h2> heading 2 </h2>
-//     </div>
-{/* <div id="child2">
-//         <h1>heading</h1>
-        //   <h2> heading 2 </h2>
-//     </div> */}
-//  </div>
- 
-const parent = React.createElement("div",{id:"parent"},
-    [
-    React.createElement("div",{id:"child"},
-        [React.createElement("h1",{},"Namaste react"),React.createElement("h2",{},"Sibling heading")]
-    ),
-    React.createElement("div",{id:"child2"},
-        [React.createElement("h1",{},"Nested heading 2"),React.createElement("h2",{},"Sibling heading 2")]
-    ),
-]
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
- 
- 
- const heading = React.createElement("h1",{id:"heading"},"Hello World in React");
+// JSX react
+// it is not HTML inside jsx
+// jsx code is transpilled before it reaches the js engine=> this is done by parcel which has a package babel which is responsible for converting jsx code to normal js code which browser can understand
+const heading = <h1 id="heading">Namaste React using JSX</h1>;
+
+const TitleComponent = ()=>{
+    return <h1 className="heading">Namaste react using jsx</h1>
+}
+
+const elem = <span>This is a element</span>;
+const title = (
+       <h1>
+        {elem}
+        This is a title
+        </h1>
+)
+
+
+const number = 1000
+// React component=> both headingcomponent are the same
+const HeadingComponent = () => (
+  <div id="container">
+     <h2>{number}</h2>
+     {title}
+    <TitleComponent/>
+    <h1 className="heading">Functional component</h1>
+  </div>
+);
+const HeadingComponent2 = () => {
+  return <h1 className="heading">Functional component</h1>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent)
 
-// root.render(heading)
+// root.render(heading);
+
+root.render(<HeadingComponent/>)
